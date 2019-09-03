fcfdbdfc940944b11a55985fa02e05ab0416dba4
LalalalavaZ <43247224+LalalalavaZ@users.noreply.github.com>
2019-09-03 16:11:23 -0400

    second

---
+++ js/oxygen.js
@@ -0,0 +1,45 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var particles = [];
+var num_particles = 208;
+
+var Particle = function () {
+    this.x = canvas.width * Math.random();
+    this.y = canvas.height * Math.random();
+    this.vx = 4 * Math.random() - 2;
+    this.vy = 4 * Math.random() - 2;
+    this.Color = "rgb(130, 190, 250)";
+}
+
+Particle.prototype.Draw = function (ctx) {
+    ctx.fillStyle = this.Color;
+    ctx.fillRect(this.x, this.y, 8, 8);
+}
+
+Particle.prototype.Update = function () {
+    this.x += this.vx;
+    this.y += this.vy;
+ 
+    if (this.x<0 || this.x > canvas.width)
+        this.vx = -this.vx;
+ 
+    if (this.y < 0 || this.y > canvas.height)
+        this.vy = -this.vy;
+}
+
+function loop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (var i = 0; i < num_particles; i++) {
+        particles[i].Update();
+        particles[i].Draw(ctx);
+    }
+    requestAnimationFrame(loop);
+}
+
+for (var i = 0; i < num_particles; i++)
+    particles.push(new Particle());
+
+loop();
